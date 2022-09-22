dbe7ffa7db8391c9016e2680c256b2b4a6743d07
Pauldwyer <pauldwyer55@gmail.com>
2022-09-22 10:35:31 +0000

    building getNewQuestion function, all working so far, questions being pulled from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices)
+console.log(choices);
 
 let currentQuestions = {};
 let acceptingAnswers = true;
 let score = 0;
-let questonCounter = 0;
-let availableQuestions = []
+let questionCounter = 0;
+let availableQuestions = [];
 
 let questions = [
     {
@@ -41,17 +41,29 @@ let questions = [
         choice4: 'ok',
         answer: 1,
     }
-]
+];
 
 // points per correct question + how many questions does the user get
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    console.log(availableQuestions)
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+// increments to 1
+    questionCounter++;
+    //random number generator
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+
 }
 
 startGame()
