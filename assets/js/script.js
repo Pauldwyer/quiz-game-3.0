315443123eba726bef70a20b73d81887e3bb6123
Pauldwyer <pauldwyer55@gmail.com>
2022-09-22 12:02:31 +0000

    fixed bug with hud code get question-counter was mispelled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
+//for targetting the hud
+const questionCounterText = document.getElementById('question-counter');
 const scoreText = document.getElementById('score');
 
 let currentQuestions = {};
@@ -64,6 +65,7 @@ getNewQuestion = () => {
     }
 // increments to 1
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
     //random number generator
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
