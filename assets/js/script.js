300a297d0aa616eb039f34e79b683aeecbefd25f
Pauldwyer <pauldwyer55@gmail.com>
2022-09-22 10:20:50 +0000

    create 5 var questions, answers, score, quest count + avail quest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 console.log(choices)
+
+let currentQuestions = {};
+let acceptingAnswers = true;
+let score = 0;
+let questonCounter = 0;
+let availableQuestions = []
