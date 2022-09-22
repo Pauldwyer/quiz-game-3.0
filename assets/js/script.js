7f6820a6431cafd9490191c3345331354a3a4755
Pauldwyer <pauldwyer55@gmail.com>
2022-09-22 10:22:32 +0000

    add 4 placeholder questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,38 @@ let acceptingAnswers = true;
 let score = 0;
 let questonCounter = 0;
 let availableQuestions = []
+
+let questions = [
+    {
+        question: 'What is 2+2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '5',
+        choice4: '66',
+        answer: 2,
+    },
+    {
+        question: 'Who is the best?',
+        choice1: 'Paul',
+        choice2: 'Viv',
+        choice3: 'Din',
+        choice4: 'Sam',
+        answer: 1,
+    },
+    {
+        question: 'Lorem Lipsum',
+        choice1: 'is',
+        choice2: 'this',
+        choice3: 'a',
+        choice4: 'placeholder',
+        answer: 3,
+    },
+    {
+        question: 'Lorem Lipsum?',
+        choice1: 'Si',
+        choice2: 'No',
+        choice3: 'Yes',
+        choice4: 'ok',
+        answer: 1,
+    }
+]
